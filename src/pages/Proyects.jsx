d8d40d07212f951832789667cc5085cd8962882f
molu <msaezcardenas@gmail.com>
2024-01-17 13:18:36 -0300

    create template of proyects landing

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from '../styles/pages/_proyects.module.scss';
+import main from '../assets/images/avatar-1.jpg';
 
 function Proyects() {
-  return <div>Proyects</div>;
+  return (
+    <div className={styles.proyects}>
+      <div className={styles.lefContainer}> Proyectos</div>
+      <div className={styles.rigthContainer}>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </div>
+  );
 }
 
 export default Proyects;
