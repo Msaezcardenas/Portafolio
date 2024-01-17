c2f54765cc2652e892db39ca7089c65ebe14a4ae
molu <msaezcardenas@gmail.com>
2024-01-17 10:54:17 -0300

    create a folder containing styles, save normalize file and main.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <h1>
+        Hola! mi nombre es Mónica Luisa o como me gusta que me llamen Molu!
+      </h1>
+      <h2> Soy desarrolladora Full-Stack y aquí me pueden conocer ! </h2>
+      <ul>
+        <li> Proyectos</li>
+        <li> Acerca de mi</li>
+      </ul>
+    </>
+  );
 }
 
 export default Home;
